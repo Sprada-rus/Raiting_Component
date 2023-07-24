67f67706e9b88a8754ae99e01a8da4316467a620
isapronov <ilya.sapronov@sum1.ru>
2023-07-24 17:42:09 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('form#rating_form');
+const submitBtn = document.querySelector('button[form="rating_form"]');
+const cardWithForm = document.querySelector('#card-with-rating');
+const thanksCard = document.querySelector('#thanks-card');
+submitBtn.classList.add('disabled');
+submitBtn.disabled = true;
+let rating = 0;
+
+const endTransitionCallback = (event) => {
+  console.log('start en event');
+  const { target } = event;
+  target.removeEventListener('transitionend', endTransitionCallback);
+  target.style.setProperty('display', 'none');
+  thanksCard.style.removeProperty('display');
+  setTimeout(() => {
+    thanksCard.classList.remove('close');
+    thanksCard.classList.add('open');
+  }, 300);
+}
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  rating = event.target?.rating?.value || 0;
+  
+  if (rating) {
+    thanksCard.querySelector('.user-rating__num').textContent = rating;
+    cardWithForm.classList.add('close');
+    cardWithForm.addEventListener('transitionend', endTransitionCallback);
+  }
+}
+
+const changeFormHandler = (event) => {
+  const formData = new FormData(event.target.form);
+
+  for (let value of formData.values()) {
+    if(!value) {
+      submitBtn.classList.add('disabled');
+      submitBtn.disabled = true;
+    }
+  }
+
+  submitBtn.classList.remove('disabled');
+  submitBtn.disabled = false;
+}
+
+form.addEventListener('change', changeFormHandler);
+form.addEventListener('submit', submitHandler);
